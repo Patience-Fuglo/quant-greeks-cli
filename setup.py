23e578a7b66388f84b247585ff71c307312bee1b
Patience-Fuglo <59480623+Patience-Fuglo@users.noreply.github.com>
2025-09-30 18:21:12 +0000

    Bump version to 1.0.1 and add long_description for PyPI project description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="quant-greeks-cli",
-    version="1.0.0",
+    version="1.0.1",
     description="A CLI tool to calculate Black-Scholes Greeks for options pricing.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Patience Fuglo",
     packages=find_packages(),
     py_modules=["greeks", "cli"],
